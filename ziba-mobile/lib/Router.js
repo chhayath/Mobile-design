4bb9d37d5ff90a7f5738ff15fb38dcb64144fe0a
chhay <chayravit@gmail.com>
2016-06-18 11:33:50 +0700

    new-project

---
+++ ziba-mobile/lib/Router.js
@@ -0,0 +1,29 @@
+
+
+Router.configure({
+	layoutTemplate:'mainLayout'
+});
+
+Router.route('/',{
+   name:'home'
+});
+
+Router.route('/addcontent',{
+   name:'addcontent'
+});
+
+Router.route('/login',{
+   name:'login'
+});
+
+Router.route('/slider',{
+   name:'slider'
+});
+
+Router.route('/title',{
+   name:'title'
+});
+
+Router.route('/titleimg',{
+   name:'titleimg'
+});
